21160aab8ec92d0024f1ca93983636796800e3e6
YannickMonga <mongassolutions@gmail.com>
2018-08-11 23:13:14 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleAppTestGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =1;
+            int b = 2;
+            int somme = a + b;
+            Console.WriteLine("la somme de a et b est" + somme);
+            Console.ReadLine();
+        }
+    }
+}
